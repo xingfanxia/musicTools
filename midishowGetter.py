8573f6cded902863154efca5bddc85ed8ec7db9d
Xingfan Xia <xiax@carleton.edu>
2018-05-21 00:59:29 -0500

    [feat] midishow script done

--- midishowGetter.py
+++ midishowGetter.py
@@ -1,5 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+# @Date    : 2018-05-21 00:57:32
+# @Author  : Xingfan Xia (xingfanxia@gmail.com)
+# @Link    : http://xiax.ai
+# @Version : 1.0
+This script downloads midi song from midishow.com
+Usage:
+    python3 midishowGetter.py <url>
+'''
+
 import wget, sys, os
 import requests, re
+import cgi
 
 outPath ="midishowDownloads/"
 
@@ -11,14 +24,13 @@ def parse_url(url):
 def main():
     url = parse_url(sys.argv[1])
     r = requests.get(url)
-    print(r.headers)
-    con_disp = r.headers.get('Content-Disposition').encode('utf-8').decode('gbk')
-    print(con_disp)
-    fname = re.findall("filename\*=(.+)", con_disp)
-    print(fname)
+    con_disp = r.headers.get('Content-Disposition').encode('ISO-8859-1').decode('utf-8')
+    v, p = cgi.parse_header(con_disp)
+    fname = p['filename*'].replace("\'", "").replace('\"', "").replace("UTF-8", "")
     if not os.path.exists(outPath):
         os.mkdir(outPath)
-    wget.download(url, outPath+con_disp)
+    open(outPath+fname, "wb").write(r.content)
+    print("{} downloaded!".format(fname))
 
 if __name__ == '__main__':
     main()
